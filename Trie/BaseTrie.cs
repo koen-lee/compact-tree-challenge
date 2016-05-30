8d7ee38b5f7e8f99835156852e511b75207f4a8f
Koen van Leeuwen <koen.lee@logiqsagro.nl>
2016-05-30 22:11:15 +0200

    Extracted common methods for this challenge

---
+++ Trie/BaseTrie.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Trie
+{
+    // An ITrie implementation that does not actually use a trie
+    abstract class BaseTrie : ITrie
+    {
+        protected static readonly Encoding Encoding = new UTF8Encoding();
+
+        protected readonly byte[] _storage; // required: this is the only field allowed
+        
+        protected BaseTrie(byte[] storage)
+        {
+            if (storage.Length > 32 * 1024) throw new ArgumentOutOfRangeException(nameof(storage), "Array too big");
+            _storage = storage;
+        }
+
+        protected string ReadString(int address, out int next)
+        {
+            int startOfString;
+            var length = ReadUshort(address, out startOfString);
+            if (length == 0)
+            {
+                next = address;
+                return string.Empty;
+            }
+            next = startOfString + length;
+            return Encoding.GetString(_storage, startOfString, length );
+        }
+
+        protected long ReadLong(int address, out int next)
+        {
+            next = address + sizeof(long);
+            return BitConverter.ToInt64(_storage, address);
+        }
+        protected ushort ReadUshort(int address, out int next)
+        {
+            next = address + sizeof(ushort);
+            return BitConverter.ToUInt16(_storage, address);
+        }
+
+        public abstract bool TryWrite(string key, long value);
+
+        protected void WriteLong(int i, long value, out int next)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            WriteBytes(i, bytes, out next);
+        }
+
+        protected void WriteUshort(int i, ushort value, out int next)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            WriteBytes(i, bytes, out next);
+        }
+
+        protected void WriteBytes(int i, byte[] bytes, out int next)
+        {
+            next = i + bytes.Length;
+            Array.Copy(bytes, 0, _storage, i, bytes.Length);
+        }
+
+        private bool WriteKeyValue(int i, string key, long value)
+        {
+            var bytes = Encoding.GetBytes(key);
+            if (i + sizeof(ushort) + bytes.Length + sizeof(long) > _storage.Length)
+                return false; // does not fit
+            WriteUshort(i, (ushort)bytes.Length, out i);
+            WriteBytes(i, bytes, out i);
+            WriteLong(i, value, out i);
+            return true;
+        }
+
+        public abstract bool TryRead(string key, out long value);
+
+        public abstract void Delete(string key);
+
+        public void Save(string filename)
+        {
+            using (var stream = new FileStream(filename, FileMode.Create, FileAccess.Write))
+            {
+                stream.Write(_storage, 0, _storage.Length);
+            }
+        }
+
+        public void Load(string filename)
+        {
+            using (var stream = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                stream.Read(_storage, 0, _storage.Length);
+            }
+        }
+    }
+}
--- Trie/NoTrie.cs
+++ Trie/NoTrie.cs
@@ -5,47 +5,15 @@ using System.Text;
 namespace Trie
 {
     // An ITrie implementation that does not actually use a trie
-    class NoTrie : ITrie
+    class NoTrie : BaseTrie
     {
-        private static readonly Encoding Encoding = new UTF8Encoding();
-
-        private readonly byte[] _storage; // required: this is the only field allowed
-
         public NoTrie() : this(new byte[32 * 1024])
         { }
 
-        public NoTrie(byte[] storage)
-        {
-            if (storage.Length > 32 * 1024) throw new ArgumentOutOfRangeException(nameof(storage), "Array too big");
-            _storage = storage;
-        }
-
-
-        private string ReadString(int address, out int next)
-        {
-            int startOfString;
-            var length = ReadUshort(address, out startOfString);
-            if (length == 0)
-            {
-                next = address;
-                return string.Empty;
-            }
-            next = startOfString + length;
-            return Encoding.GetString(_storage, startOfString, length );
-        }
-
-        private long ReadLong(int address, out int next)
-        {
-            next = address + sizeof(long);
-            return BitConverter.ToInt64(_storage, address);
-        }
-        private ushort ReadUshort(int address, out int next)
-        {
-            next = address + sizeof(ushort);
-            return BitConverter.ToUInt16(_storage, address);
-        }
-
-        public bool TryWrite(string key, long value)
+        public NoTrie(byte[] storage): base(storage)
+        { }
+        
+        public override bool TryWrite(string key, long value)
         {
             if (string.IsNullOrEmpty(key)) return false; // not in requirements
             int i = 0;
@@ -63,25 +31,7 @@ namespace Trie
             }
             return false;
         }
-
-        private void WriteLong(int i, long value, out int next)
-        {
-            var bytes = BitConverter.GetBytes(value);
-            WriteBytes(i, bytes, out next);
-        }
-
-        private void WriteUshort(int i, ushort value, out int next)
-        {
-            var bytes = BitConverter.GetBytes(value);
-            WriteBytes(i, bytes, out next);
-        }
-
-        private void WriteBytes(int i, byte[] bytes, out int next)
-        {
-            next = i + bytes.Length;
-            Array.Copy(bytes, 0, _storage, i, bytes.Length);
-        }
-
+         
         private bool WriteKeyValue(int i, string key, long value)
         {
             var bytes = Encoding.GetBytes(key);
@@ -93,7 +43,7 @@ namespace Trie
             return true;
         }
 
-        public bool TryRead(string key, out long value)
+        public override bool TryRead(string key, out long value)
         {
             value = -1L;
             int i = 0;
@@ -112,7 +62,7 @@ namespace Trie
             return false;
         }
 
-        public void Delete(string key)
+        public override void Delete(string key)
         {
             int i = 0;
             while (i < _storage.Length)
@@ -131,22 +81,6 @@ namespace Trie
                     Array.Copy(newStorage, 0, _storage, 0, _storage.Length);
                     return;
                 }
-            }
-        }
-
-        public void Save(string filename)
-        {
-            using (var stream = new FileStream(filename, FileMode.Create, FileAccess.Write))
-            {
-                stream.Write(_storage, 0, _storage.Length);
-            }
-        }
-
-        public void Load(string filename)
-        {
-            using (var stream = new FileStream(filename, FileMode.Open, FileAccess.Read))
-            {
-                stream.Read(_storage, 0, _storage.Length);
             }
         }
     }
