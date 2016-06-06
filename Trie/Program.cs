dca6fe6ba1481b6c1405743c956e17ae4ba4f2b3
Koen van Leeuwen <koen.lee@logiqsagro.nl>
2016-06-06 20:16:07 +0200

    Created console app for profiling

---
+++ Trie/Program.cs
@@ -0,0 +1,21 @@
+namespace Trie
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tester = new TrieComparisonTests();
+            tester.GenerateTestData();
+            tester.TestOptimizedTrie();
+            tester.TestOptimizedTrie();
+            tester.TestOptimizedTrie();
+            tester.TestOptimizedTrie();
+            tester.TestOptimizedTrie();
+            tester.TestOptimizedTrie();
+            tester.TestOptimizedTrie();
+            tester.TestOptimizedTrie();
+            tester.TestOptimizedTrie();
+            tester.TestOptimizedTrie();
+        }
+    }
+}
