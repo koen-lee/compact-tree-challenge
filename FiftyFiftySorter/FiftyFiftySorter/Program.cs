f628506c2ac86083f126ccdb045af8042e248629
Koen van Leeuwen <koen.lee@logiqsagro.nl>
2017-07-05 11:52:14 +0200

    Simple puzzle for junior programmers

---
+++ FiftyFiftySorter/FiftyFiftySorter/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FiftyFiftySorter
+{
+    public enum Destination
+    {
+        Left,
+        Right
+    }
+
+    static class Program
+    {
+        struct Plant
+        {
+            public int Id;
+            public int Height;
+            public Destination Destination;
+
+            public override string ToString()
+            {
+                return $"Id: {Id}, Height: {Height}, Destination: {Destination}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var random = GetRandomizer(args);
+            try
+            {
+                while (!Console.KeyAvailable)
+                    RunSimulation(random);
+            }
+            catch (Exception e)
+            {
+                WriteLine(ConsoleColor.Red, e);
+            }
+            Console.ReadLine();
+        }
+
+        private static void RunSimulation(Random random)
+        {
+            var sorter = new Sorter();
+
+            var plants = SortPlants(random, sorter);
+            ReportResults(plants);
+        }
+
+        private static List<Plant> SortPlants(Random random, Sorter sorter)
+        {
+            var min = random.Next(0, 1000);
+            var result = new List<Plant>(100);
+            for (int plant = 0; plant < 100; plant++)
+            {
+                var height = min + random.Next(100);
+                var destination = sorter.GetDestinationForPlant(height);
+                var item = new Plant
+                {
+                    Id = plant,
+                    Destination = destination,
+                    Height = height
+                };
+                result.Add(item);
+            }
+            return result;
+        }
+
+        private static void ReportResults(List<Plant> plants)
+        {
+            var count = plants.Count(p => p.Destination == Destination.Left);
+            var expectedCount = 50;
+            if (count != expectedCount)
+            {
+                throw new Exception($"Expected {expectedCount} to Left, but was {count}");
+            }
+            var smallestPlants = plants.OrderBy(p => p.Height).Take(expectedCount);
+            var sortedCorrectly = smallestPlants.Count(p => p.Destination == Destination.Left);
+            WriteLine(ConsoleColor.Green, $"Sort quality: {sortedCorrectly}/{expectedCount}");
+        }
+
+        private static Random GetRandomizer(string[] args)
+        {
+            if (args.Length == 0)
+                return new Random();
+            return new Random(args[0].GetHashCode());
+        }
+
+        private static void WriteLine(ConsoleColor color, object text)
+        {
+            var oldcolor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = oldcolor;
+        }
+    }
+}
---
+++ FiftyFiftySorter/FiftyFiftySorter/Sorter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FiftyFiftySorter
+{
+    class Sorter
+    {
+        public Destination GetDestinationForPlant(int height)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
