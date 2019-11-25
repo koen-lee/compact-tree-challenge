213d9cf1c34e19357b688e7846524d3e49957c24
Koen van Leeuwen <koen@logiqs.nl>
2019-11-25 12:09:10 +0100

    Port to .net core

--- Garage/Garage/Program.cs
+++ Garage/Garage/Program.cs
@@ -1,40 +1,41 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 
 namespace Garage
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            AppDomain.MonitoringIsEnabled = true;
             var sp = Stopwatch.StartNew();
 
             const string path = @"data.txt";
             CreateSummaryOptimized(path);
 
-            Console.WriteLine($"Took: {sp.ElapsedMilliseconds:#,#} ms and allocated {AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize / 1024:#,#} kb with peak working set of {Process.GetCurrentProcess().PeakWorkingSet64 / 1024:#,#} kb");
+            Console.WriteLine($"Took: {sp.ElapsedMilliseconds:#,#} ms with peak working set of {Process.GetCurrentProcess().PeakWorkingSet64 / 1024:#,#} kb");
         }
 
         const int DICT_BLOCK_SIZE = 1000;
         const int recordsize = 50;
+        static int[][] dict = new int[9999999 + 1 / DICT_BLOCK_SIZE][];
 
         private static void CreateSummaryOptimized(string path)
         {
-            var dict = new int[9999999 + 1 / DICT_BLOCK_SIZE][];
             var file = new FileInfo(path);
 
             var record = new byte[1000 * recordsize];
             using (var records = file.OpenRead())
             {
-                for (;;)
+                for (; ; )
                 {
                     var bytesRead = records.Read(record, 0, record.Length);
                     if (bytesRead <= 0) break;
                     for (int i = 0; i < bytesRead; i += recordsize)
-                        ProcessLine(record, i, dict);
+                    {
+                        var span = new ReadOnlySpan<byte>(record, i, recordsize);
+                        ProcessLine(span);
+                    }
                 }
             }
             using (var output = File.CreateText("summary.txt"))
@@ -53,29 +54,29 @@ namespace Garage
             }
         }
 
-        private static void ProcessLine(byte[] record, int offset, int[][] dict)
+        private static void ProcessLine(ReadOnlySpan<byte> record)
         {
             int durationSeconds;
-            if (!BufferEqual(record, startIndex1: offset + 0, startIndex2: offset + 20, length: 10)) // Same day check
+            if (!BufferEqual(record.Slice(0, 10), record.Slice(20, 10)))
             {
-                var start = ParseDateTime(record, offset + 0);
-                var end = ParseDateTime(record, offset + 20);
+                var start = ParseDateTime(record, 0);
+                var end = ParseDateTime(record, 20);
                 durationSeconds = (int)(end - start).TotalSeconds;
             }
             else
             {
-                var start = ParseTime(record, index: offset + 11);
-                var end = ParseTime(record, index: offset + 31);
+                var start = ParseTime(record, index: 11);
+                var end = ParseTime(record, index: 31);
                 durationSeconds = end - start;
             }
-            var id = ParseInt(record, startIndex: offset + 40, length: 8);
+            var id = ParseInt(record, startIndex: 40, length: 8);
             int[] dictionary = dict[id / DICT_BLOCK_SIZE];
             //Interlocked.Add(ref dict[id], durationSeconds);
             if (dictionary == null) dict[id / DICT_BLOCK_SIZE] = dictionary = new int[DICT_BLOCK_SIZE];
             dictionary[id % DICT_BLOCK_SIZE] += durationSeconds;
         }
 
-        private static DateTime ParseDateTime(byte[] record, int offset)
+        private static DateTime ParseDateTime(ReadOnlySpan<byte> record, int offset)
         {
             return new DateTime(kind: DateTimeKind.Utc,
                 year: ParseInt(record, offset, 4),
@@ -93,7 +94,7 @@ namespace Garage
         /// <param name="record"></param>
         /// <param name="index"></param>
         /// <returns></returns>
-        private static int ParseTime(byte[] record, int index)
+        private static int ParseTime(ReadOnlySpan<byte> record, int index)
         {
             //hours
             var result = ParseUint(record, index);
@@ -104,14 +105,14 @@ namespace Garage
             return result;
         }
 
-        private static int ParseUint(byte[] record, int startindex)
+        private static int ParseUint(ReadOnlySpan<byte> record, int startindex)
         {
             var result = record[startindex] - '0';
             result = result * 10 + record[startindex + 1] - '0';
             return result;
         }
 
-        private static int ParseInt(byte[] record, int startIndex, int length)
+        private static int ParseInt(ReadOnlySpan<byte> record, int startIndex, int length)
         {
             var result = 0;
             for (var i = startIndex; i < startIndex + length; i++)
@@ -120,19 +121,9 @@ namespace Garage
             }
             return result;
         }
-
-        private static bool BufferEqual(byte[] record, int startIndex1, int startIndex2, int length)
+        private static bool BufferEqual(ReadOnlySpan<byte> slice1, ReadOnlySpan<byte> slice2)
         {
-            for (int i = length - 1; i >= 0; i--)
-            {
-                if (record[startIndex1 + i] != record[startIndex2 + i]) return false;
-            }
-            return true;
-        }
-
-        static string ToString(byte[] buffer)
-        {
-            return Encoding.UTF8.GetString(buffer);
+            return slice1.SequenceCompareTo(slice2) == 0;
         }
     }
 }
